2c19900de589090b0c66dd616a71a489d8916c58
alex <golovkobalak@gmail.com>
2019-04-21 12:26:43 +0300

    testing am2320

--- src/main/java/com/golovko/rpi/model/AM2320TemperatureAndHumidity.java
+++ src/main/java/com/golovko/rpi/model/AM2320TemperatureAndHumidity.java
@@ -13,6 +13,8 @@ import java.util.Map;
 import java.util.Properties;
 
 public class AM2320TemperatureAndHumidity implements Detectable {
+    private final String HUMIDITY = "humidity";
+    private final String TEMPERATURE = "temperature";
     static Properties pythonScriptsAm2320;
     private final String RUN = "run";
     private final Logger logger = LoggerFactory.getLogger(AM2320TemperatureAndHumidity.class);
@@ -33,82 +35,51 @@ public class AM2320TemperatureAndHumidity implements Detectable {
     }
 
 
-    public Map<String, Double> getTemperatureAndHumidity() {
+    public Map<String, Double> getTemperatureAndHumidity() throws IOException {
         Process process;
         try {
 
             process = Runtime.getRuntime().exec(pythonScriptsAm2320.getProperty(RUN));
         } catch (IOException e) {
             e.printStackTrace();
+            process = Runtime.getRuntime().exec("sudo python am2320.py");
 
-            try {
-                process = Runtime.getRuntime().exec("sudo python am2320.py");
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                return null;
-            }
         }
         try {
             process.waitFor();
-
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         StringBuilder resultBuilder = new StringBuilder();
-        try {
-            InputStream inputStream = process.getInputStream();
-            logger.info("AVAILABLE CHARS======"+inputStream.available()    );
-            /*for (int i = 0; i < inputStream.available(); i++) {
-                resultBuilder.append((char) inputStream.read());
-            }*/
-            int i;
-            while ((i=inputStream.read())!=-1){
-                resultBuilder.append((char)i);
-                logger.info("NEXT CHAR===="+(char)i);
-            }
-            logger.info("WHILE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+resultBuilder.toString()+"<<<<<<<<<<<<<<<<<<<<<<<<,");
-        } catch (IOException e) {
-
-            e.printStackTrace();
+        InputStream inputStream = process.getInputStream();
+        int i;
+        while ((i = inputStream.read()) != -1) {
+            resultBuilder.append((char) i);
         }
+        inputStream.close();
 
-        String resultString = resultBuilder.toString();
-        System.out.println("RESULT siot="+resultString);
-        logger.debug("RESULT String>>>>>>>>>>>>>>>>>>.'"+resultString+"'<<<<<<<<<<<<<<<<<<<<<<<<<==================");
-        Double temperature = Double.valueOf(resultString.substring(0, resultString.indexOf(" ")));
-        Double humidity = Double.valueOf(resultString.substring(resultString.lastIndexOf(" ", resultString.length() - 1)));
+        Double temperature = Double.valueOf(resultBuilder.substring(0, resultBuilder.indexOf(" ")));
+        Double humidity = Double.valueOf(resultBuilder.substring(resultBuilder.lastIndexOf(" ")));
         Map<String, Double> result = new HashMap<>();
-        result.put("temperature", temperature);
-        result.put("humidity", humidity);
+        result.put(TEMPERATURE, temperature);
+        result.put(HUMIDITY, humidity);
 
         return result;
     }
 
 
     public double getHumidity() throws IOException {
-        double humidity;
-        try {
-            Process run = Runtime.getRuntime().exec(pythonScriptsAm2320.getProperty("run"));
-            run.waitFor();
-            InputStream inputStream = run.getInputStream();
-            if (inputStream.available() > 0) {
 
-            }
-        } catch (Exception e) {
 
-        }
-        return 0d;
+        return getTemperatureAndHumidity().get(HUMIDITY);
     }
 
     ;
 
-    public double getTemperature() {
-        return 0d;
+    public double getTemperature() throws IOException {
+        return getTemperatureAndHumidity().get(TEMPERATURE);
     }
 
-    ;
 
     @Override
     public Gson getData() {
--- src/test/java/com/golovko/rpi/model/AM2320Test.java
+++ src/test/java/com/golovko/rpi/model/AM2320Test.java
@@ -1,19 +1,55 @@
 package com.golovko.rpi.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class AM2320Test {
+    AM2320TemperatureAndHumidity am2320;
+
+    @Before
+    public void setUp() {
+        am2320 = new AM2320TemperatureAndHumidity();
+    }
 
     @Test
     public void shutGetTemperatureAndHumidity() {
-        AM2320TemperatureAndHumidity am2320=new AM2320TemperatureAndHumidity();
-        Map<String, Double> temperatureAndHumidity = am2320.getTemperatureAndHumidity();
+        Map<String, Double> temperatureAndHumidity = null;
+        try {
+            temperatureAndHumidity = am2320.getTemperatureAndHumidity();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         assertNotNull(temperatureAndHumidity);
-        assertTrue(temperatureAndHumidity.get("temperature")>0);
-        assertTrue(temperatureAndHumidity.get("humidity")>0);
+        assertTrue(temperatureAndHumidity.get("temperature") > 0);
+        assertTrue(temperatureAndHumidity.get("humidity") > 0);
+    }
+
+    @Test
+    public void shutGetTemperature() {
+        double temperature = Double.MIN_VALUE;
+        try {
+            temperature = am2320.getTemperature();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(temperature > -50 && temperature < 100);
+    }
+
+    @Test
+    public void shutGetHumidity() {
+        double humidity = Double.MIN_VALUE;
+        try {
+            humidity = am2320.getHumidity();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(humidity > -50 && humidity < 100);
+
     }
 }
