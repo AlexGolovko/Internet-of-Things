8b15fcb2c3fa00277b6b2ebc8cc8f8a132752cd0
alex <golovkobalak@gmail.com>
2019-04-22 18:40:57 +0300

    testing am2320

--- src/main/java/com/golovko/rpi/InternetOfThingsOnRpiApplication.java
+++ src/main/java/com/golovko/rpi/InternetOfThingsOnRpiApplication.java
@@ -1,7 +1,6 @@
 package com.golovko.rpi;
 
 import com.golovko.rpi.controller.ConfigSensors;
-import com.golovko.rpi.controller.RaspiController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,7 +13,7 @@ public class InternetOfThingsOnRpiApplication {
     public static void main(String[] args) {
         SpringApplication.run(InternetOfThingsOnRpiApplication.class, args);
         ApplicationContext controllerContext=new AnnotationConfigApplicationContext(ConfigSensors.class);
-        RaspiController raspiController=controllerContext.getBean(RaspiController.class);
+       // RaspiController raspiController=controllerContext.getBean(RaspiController.class);
     }
 
 
